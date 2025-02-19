d9fbec6262dd01344e05d8e2058bbaa29c26fe1a
Danish <dkhans2001@gmail.com>
2025-02-18 21:09:32 -0600

    function

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hq = 42;
+    return Math.abs(hq - someValue);
+}
+
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+// Returns the number of feet traveled between two locations
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    //returns the fare for the customer
+    if (distance <= 400) {
+      return 0;
+  } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+  } else {
+      return 'cannot travel that far';
+  }
+}
